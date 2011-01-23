238cc0e48f96c6ca2e736bd53836e2a40f7b80a9
Sven A. Schmidt <sas@abstracture.de>
2011-01-23 20:41:41 +0100

    Added new root view controller

---
+++ Classes/RootViewController.h
@@ -0,0 +1,17 @@
+//
+//  RootViewController.h
+//  AbsReader
+//
+//  Created by Sven A. Schmidt on 23.01.11.
+//  Copyright 2011 abstracture GmbH & Co. KG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UITableViewController {
+@private
+    
+}
+
+@end
