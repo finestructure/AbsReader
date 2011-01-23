a25593c7fd85ebe04f1c6ba444a7063d33c50272
Sven A. Schmidt <sas@abstracture.de>
2011-01-23 20:47:51 +0100

    Renamed ArticleCacheDelegate to FeedCacheDelegate

--- Classes/FeedCache.h
+++ Classes/FeedCache.h
@@ -8,12 +8,12 @@
 
 #import <Foundation/Foundation.h>
 
-@protocol ArticleCacheDelegate;
+@protocol FeedCacheDelegate;
 
 @interface FeedCache : NSObject<NSXMLParserDelegate> {
 }
 
-@property (nonatomic, assign) id<ArticleCacheDelegate> delegate;
+@property (nonatomic, assign) id<FeedCacheDelegate> delegate;
 @property (nonatomic, retain) NSMutableDictionary *cache;
 @property (nonatomic, retain) NSMutableArray *stories;
 @property (nonatomic, retain) NSXMLParser *rssParser;
@@ -33,7 +33,7 @@
 
 @end
 
-@protocol ArticleCacheDelegate<NSObject>
+@protocol FeedCacheDelegate<NSObject>
 
 - (void)errorOccurred:(NSError *)error;
 - (void)didEndDocument;
--- Classes/FeedViewController.h
+++ Classes/FeedViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import "FeedCache.h"
 
-@interface FeedViewController : UITableViewController<ArticleCacheDelegate> {
+@interface FeedViewController : UITableViewController<FeedCacheDelegate> {
   IBOutlet UITableView *newsTable;
 }
 
