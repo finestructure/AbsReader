dbc0e25ad003beb78f02dd30ae3814e18f446f45
Sven A. Schmidt <sas@abstracture.de>
2011-02-18 09:56:24 +0100

    Updated UIAutomation tests somewhat. Not yet in a state where this would be useful. Issues: incomplete sandboxing: Application retains state even between restarts. Accessibility names are not seen, neither when set via IB nor via code.

--- AutomationTests/test.js
+++ AutomationTests/test.js
@@ -1,10 +1,40 @@
 #import "../tuneup/tuneup.js"
 
-test("Refresh button", function(target, app) {
+// mainWindow.logElementTree();
+// UIATarget.localTarget().captureScreenWithName("refresh_all");
+
+UIATarget.onAlert = function onAlert(alert) {
+  var title = alert.name();
+  UIALogger.logWarning("Alert with title '" + title + "' encountered!");
+
+  if (title == "Error fetching feed") {
+    alert.buttons()["Continue"].tap();
+    return true; // bypass default handler
+  }
+  return false; // use default handler
+}
+
+test("Add feed", function(target, app) {
+  mainWindow = app.mainWindow();
+  navBar = mainWindow.navigationBar();
+  addButton = navBar.buttons()[1];
+  addButton.tap();
+  mainWindow.textFields()["title"].setValue("Test Feed")
+  mainWindow.textFields()["url"].setValue("http://test.com")
+  cancelButton = navBar.buttons()[0];
+  cancelButton.tap();
+});
+
+
+/*
+test("Refresh all", function(target, app) {
   mainWindow = app.mainWindow();
   navBar = mainWindow.navigationBar();
   assertEquals("AbsReader", navBar.name());
-  refreshButton = navBar.leftButton();
+  toolbar = mainWindow.toolbar();
+  refreshButton = toolbar.buttons()[1];
   refreshButton.tap()
-  assertEquals("AbsReader", navBar.name());
 });
+*/
+
+
