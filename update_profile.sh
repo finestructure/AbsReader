974b81fb395c9ca609e25f71f78c260ecb0d7f68
Sven A. Schmidt <sas@abstracture.de>
2011-12-19 17:46:21 +0100

    Fixed path

--- update_profile.sh
+++ update_profile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 publishing_target="abslogin:~/public_html/AbsReader/"
-profile=iOS_Team_Provisioning_Profile_.mobileprovision
+profile=releases/iOS_Team_Provisioning_Profile_.mobileprovision
 
 scp $profile $publishing_target
