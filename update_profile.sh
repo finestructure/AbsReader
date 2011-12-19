af776fc733ecc968c9043db576d9add076d56156
Sven A. Schmidt <sas@abstracture.de>
2011-12-19 17:45:28 +0100

    Fixed provisioning profile file name

--- update_profile.sh
+++ update_profile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 publishing_target="abslogin:~/public_html/AbsReader/"
-profile=Team_Provisioning_Profile_.mobileprovision
+profile=iOS_Team_Provisioning_Profile_.mobileprovision
 
 scp $profile $publishing_target
